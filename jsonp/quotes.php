62e7e80dfd64116d14007d27edc003c447a59942
mamengyi <708833607@qq.com>
2016-10-07 16:55:14 +0800

    jsonp

--- jsonp/quotes.php
+++ jsonp/quotes.php
@@ -1,17 +1,6 @@
 <?php
-header('Content-type: application/json');
+$arr=array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+$json_data = json_encode($arr);
 $jsoncallback = $_GET ['jsoncallback'];
-$json_data = '[{
-		"quote":"If you can\'t get rid of the skeleton in your closet, you\'d best teach it to dance.",
-		"author":"George Bernard Shaw"
-	}
-	{
-		"quote":"We\'ll always have Paris.",
-		"author":"Casablanca"
-	}
-	{
-		"quote":"A mathematician is a device for turning coffee into theorems.",
-		"author":"Paul Erdos"
-	}]';
 echo $jsoncallback . "(" . $json_data . ")";
 ?>
